a2e198b51fe7788d608b55c3251ca4363128ff78
Noritaka Horio <holy.shared.design@gmail.com>
2010-05-03 14:48:08 +0900

    marker images changed.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- Source/MMap.Marker.Images.js
+++ Source/MMap.Marker.Images.js
@@ -1,25 +1,60 @@
 MMap.Marker.Images = new Class({
 
+	Extends: MMap.Marker,
+
 	options: {
 		"className": "square",
 		"latlng": null,
-		"title": null,
-		"url": null,
-		"src": null
+		"images": [],
+		"interval": 600
 	},
 
 	initialize: function(map, options) {
 		this.parent(map, options);
+		this.current = 0;
+		this.intervalID = 0;
+		this.addEvent("mouseover", function(event) {
+			this.intervalID = this.next.periodical(300, this);
+		}.bind(this));
+		this.addEvent("mouseout", function(event) {
+			$clear(this.intervalID);
+		}.bind(this));
+		this.activate();
 	},
 
 	build: function() {
-		var container = this.getContainer();
-		this.photo = new Element("p", {"class": "photo"});
-		this.trriger = new Element("a", {"title": this.options.title, "href": this.options.url});
-		this.image = new Element("img", {"src": this.options.src});
-		this.image.inject(this.trriger);
-		this.trriger.inject(this.photo);
-		this.photo.inject(container);
+		this.container.addClass("image");
+		this.imageList = new Element("ul");
+		this.buildImages();
+		this.trigger = $(this.imageList).getElements("a");
+		this.setContent(this.imageList);
+	},
+
+	buildImages: function() {
+		var images = this.options.images;
+		images.each(function(image) {
+			var container = new Element("li");
+			var trigger = new Element("a", {"title": image.title, "href": image.url});
+			var photo = new Element("img", {"title": image.title, "src": image.src});
+			photo.inject(trigger);
+			trigger.inject(container);
+			container.inject(this.imageList);
+		}, this);
+	},
+
+	next: function() {
+		((this.current + 1) >= this.trigger.length)
+		? this.current = 0
+		: this.current++;
+		this.activate();
+	},
+
+	activate: function() {
+		this.trigger.each(function(image, key){
+			(this.current == key)
+			? image.setStyle("display", "")
+			: image.setStyle("display", "none");
+		}, this);
 	}
 
 });
