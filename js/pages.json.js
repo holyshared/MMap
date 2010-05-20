81f7ebe46a9c4f52f685bae69d2c1adcfd6662b1
Noritaka Horio <holy.shared.design@gmail.com>
2010-05-20 22:25:47 +0900

    marker chnaged.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- js/pages.json.js
+++ js/pages.json.js
@@ -6,20 +6,22 @@ Pages.JSON = {
 
 	"options": {
 		"center": {"lat": 35.665326, "lng": 139.726325},
-		"zoom": 12,
+		"zoom": 13,
 		"mapType": "roadmap"
 	},
 
 	"markers": [
 		{
-			"latlng": {"lat": 35.665326, "lng": 139.726325}, "title": null,
-			"content": "aaaaaaaaaaaaa"
+			"latlng": {"lat": 35.665326, "lng": 139.726325}, "title": "aaaaaaaaaaaa",
+			"content": "Some text Some text Some text Some text<br />Some text Some text Some text Some text",
+			"onClick": this.onMarkerClick
 		},
 		{
 			"latlng": {"lat": 35.621822, "lng": 139.735863},
 			"title": "Tokyo ART Center",
 			"url": "http://holyshared.github.com/MMap/",
-			"src": "http://holyshared.github.com/MMap/images/index/img_event1.jpg"
+			"src": "http://holyshared.github.com/MMap/images/index/img_event1.jpg",
+			"onClick": this.onMarkerClick
 		},
 		{
 			"latlng": {"lat": 35.682951, "lng": 139.686895},
@@ -29,7 +31,8 @@ Pages.JSON = {
 {"title": "Tokyo ART Center", "url": "http://holyshared.github.com/MMap/", "src": "http://holyshared.github.com/MMap/images/index/img_event4.jpg"},
 {"title": "Tokyo ART Center", "url": "http://holyshared.github.com/MMap/", "src": "http://holyshared.github.com/MMap/images/index/img_event5.jpg"}
 			],
-			"interval": 1000
+			"interval": 1000,
+			"onClick": this.onMarkerClick
 		}
 	],
 
@@ -40,7 +43,17 @@ Pages.JSON = {
 
 	setupInterface: function() {
 		this.map = new MMap($("map"), this.options);
+
+		for (var i = 0; i < this.markers.length; i++) {
+			this.markers[i].onClick = this.onMarkerClick;
+		}
 		this.map.loadJSON(this.markers);
+	},
+
+	onMarkerClick: function(event) {
+alert(event.target);
+		this.orderToFront();
+		this.map.setCenter(this.getPosition());
 	}
 
 };
