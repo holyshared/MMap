f4fed441277df40e3e139d12152164c24686587a
holy <holy.shared.design@gmail.com>
2010-12-02 13:51:35 +0900

    Test files updated.
    Signed-off-by: holy <holy.shared.design@gmail.com>

---
+++ Tests/js/mvcobject.js
@@ -0,0 +1,9 @@
+(function($){
+
+	window.addEvent('domready', function(){
+		var logger = new Logger();
+		var object = new MMap.MVCObject();
+		object.set('visible', true);
+	});
+
+}(document.id));
