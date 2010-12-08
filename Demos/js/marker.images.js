747d8ff1d732846f75d424d6ed620f14cbe40998
Noritaka Horio <holy.shared.design@gmail.com>
2010-12-09 01:09:44 +0900

    add Marker.Images demo page [#16]
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

---
+++ Demos/js/marker.images.js
@@ -0,0 +1,42 @@
+(function($){
+
+window.addEvent('domready', function(){
+
+	var map = new google.maps.Map($('gmap'), {
+		zoom: 15,
+		center: new google.maps.LatLng(35.6666870, 139.731859),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	var marker = new MMap.Marker.Images({
+		map: map,
+		className: 'marker images imagesPaper',
+		position: new google.maps.LatLng(35.6666870, 139.731859),
+		images: [{
+			title: 'demo1',
+			image: '../Demos/images/demo/img01.jpg',
+			url: 'http://sharedhat.com'
+		}, {
+			title: 'demo2',
+			image: '../Demos/images/demo/img02.jpg',
+			url: 'http://sharedhat.com'
+		}, {
+			title: 'demo3',
+			image: '../Demos/images/demo/img03.jpg',
+			url: 'http://sharedhat.com'
+		}, {
+			title: 'demo4',
+			image: '../Demos/images/demo/img04.jpg',
+			url: 'http://sharedhat.com'
+		}, {
+			title: 'demo5',
+			image: '../Demos/images/demo/img05.jpg',
+			url: 'http://sharedhat.com'
+		}]
+	});
+
+	SyntaxHighlighter.all();
+
+});
+
+}(document.id));
