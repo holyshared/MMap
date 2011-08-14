0b5e3a3ecd425cc1102b7d6ef9cc62b7c0e5d413
Noritaka Horio <holy.shared.design@gmail.com>
2011-08-14 09:01:22 +0900

    add MMap.Marker.Draggable.js

---
+++ Source/Marker/MMap.Marker.Draggable.js
@@ -0,0 +1,19 @@
+/*
+---
+name: MMap.Marker.Draggable
+
+description: Enhancing to be able to do drag and drop is built into the marker.
+
+license: MIT-style
+
+authors:
+- Noritaka Horio
+
+requires:
+  - MMap/MMap
+  - MMap/MMap.Marker.Core
+
+provides: [MMap.Marker.Draggable]
+
+...
+*/
